99a3b6f2a43f106e03a81ad523880e1e3cc3fc6c
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-02-06 13:25:45 +0600

    Time: 292 ms (83.70%) | Memory: 85 MB (67.71%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        unordered_map<int, int> productCount;
+        unordered_map<int, int> product;
         int ans = 0, n = nums.size();
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int product = nums[i] * nums[j];
-                ans += productCount[product]++; // Adds combinations directly
+                int prod = nums[i] * nums[j];
+                ans += product[prod]++;
             }
         }
-        return ans * 8; // Each tuple can be arranged in 8 ways
+        return ans * 8;
     }
 };
