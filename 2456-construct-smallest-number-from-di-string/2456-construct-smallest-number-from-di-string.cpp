c4e13de7beb862315b8cbde7299a19a0ecc2301d
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-02-18 14:12:02 +0600

    Time: 0 ms (100%), Space: 8.5 MB (27.59%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    bool ck(string s, string p) {
-        for (int i = 0; i < p.size(); i++) {
-            if (p[i] == 'I' and s[i] > s[i + 1]) {
-                return false;
-            }
-            else if (p[i] == 'D' and s[i] < s[i + 1]) {
-                return false;
-            }
+    string smallestNumber(string pattern) {
+        string result = "";
+        stack<int> st;
 
+        for (int i = 0; i <= pattern.size(); i++) {
+            st.push(i + 1);
+
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result += to_string(st.top());
+                    st.pop();
+                }
+            }
         }
-        return true;
-    }
-    string smallestNumber(string p) {
-        string n = "123456789", s = "";
-        for (int i = 0; i <= p.size(); i++) {
-            s += n[i];
-        }
-        bool f = false;
-        string ans;
 
-        do {
-            if (ck(s, p)) {
-                f = true;
-                cout << s << endl;
-                ans=s;
-                break;
-            }
-            // cout << s << endl;
-
-        } while (next_permutation(s.begin(), s.end()));
-        return ans;
+        return result;
     }
 };
