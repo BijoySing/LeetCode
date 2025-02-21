51737d60eb00e55fe4c821bd880add0b705ea941
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-02-21 08:43:04 +0600

    Time: 7 ms (82.43%), Space: 27.5 MB (74.39%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -2,19 +2,23 @@ class FindElements {
 public:
     unordered_set<int> values;
 
-    void recover(TreeNode* root, int val) {
-        if (!root)
-            return;
-        root->val = val;
-        values.insert(val);
-        recover(root->left, 2 * val + 1);
-        recover(root->right, 2 * val + 2);
-    }
-
     FindElements(TreeNode* root) {
-        if (root)
-            recover(root, 0);
+        if (!root) return;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto [node, val] = q.front();
+            q.pop();
+            node->val = val;
+            values.insert(val);
+
+            if (node->left) q.push({node->left, 2 * val + 1});
+            if (node->right) q.push({node->right, 2 * val + 2});
+        }
     }
 
-    bool find(int target) { return values.count(target); }
+    bool find(int target) {
+        return values.count(target);
+    }
 };
