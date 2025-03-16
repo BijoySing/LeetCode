29aadc74f57df426048e14f69d236df7e322d8b7
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-03-16 11:52:28 +0600

    Time: 0 ms (100%), Space: 29.4 MB (0%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        int sum=0;
-        set<int>st;
-        int mxVal=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-          if(nums[i]>0)st.insert(nums[i]);
-          else mxVal=max(mxVal,nums[i]);
+        int sum = 0;
+        set<int> st;
+        int mxNeg = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0)
+                st.insert(nums[i]);
+            else
+                mxNeg = max(mxNeg, nums[i]);
         }
-        for(auto val:st){
+        for (auto val : st) {
 
-          sum+=val;
+            sum += val;
         }
-        if(st.size())return sum;
-        else return mxVal;
-
+        if (st.size())
+            return sum;
+        else
+            return mxNeg;
     }
 };
