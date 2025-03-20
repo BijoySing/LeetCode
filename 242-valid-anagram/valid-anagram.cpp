8a9a935d57146ab3af71f99ed3c87e080e65c9e6
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-03-20 22:37:23 +0600

    Time: 3 ms (54.78%) | Memory: 9.6 MB (74.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>m1,m2;
-        for(auto u:s)m1[u]++;
-        for(auto u:t)m2[u]++;
-        if(s.size() !=t.size())return false;
-        for(auto [x,y]:m1){
-          if(m1[x]!=m2[x])return false;
-        }
-        return true;
+         if (s.size() != t.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
