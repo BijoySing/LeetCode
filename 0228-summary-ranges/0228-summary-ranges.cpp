6a66c590c4b770005375e883409118ae2280f3df
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-03-21 16:36:09 +0600

    Time: 0 ms (100%), Space: 9.1 MB (94.37%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    vector<string> summaryRanges(vector<int>& arr) {
-        int n = arr.size(); // extracting size of the array
-        vector<string> ans; // declaring answer array to store our answer
-        
-        string temp = ""; // temproray string that stores all possible answer
-        
-        for(int i = 0; i < n; i++) // start traversing from the array
-        {
-            int j = i; // declare anthor pointer that will move
-            
-            // run that pointer until our range is not break
-            while(j + 1 < n && arr[j + 1] == arr[j] + 1)
-            {
-                j++;
+    vector<string> summaryRanges(vector<int>& a) {
+        vector<string> v;
+        int k = 0;
+        string x = "->";
+        int n = a.size();
+        for (int i = 0; i < a.size(); i++) {
+            string s = "";
+            if (i < n - 1 and a[i] == a[i + 1] - 1) {
+                k++;
+            } else {
+               if(k) s += (to_string(a[i] - k)),
+                s += x;
+                s += (to_string(a[i]));
+                k = 0;
+                v.push_back(s);
             }
-            
-            // if j > i, that means we got our range more than one element
-            if(j > i)
-            {
-                temp += to_string(arr[i]); // first store starting point
-                temp += "->"; // then store arrow, as question wants it
-                temp += to_string(arr[j]); // and lastly store the end point
-            }
-            else // we got only one element as range
-            {
-                temp += to_string(arr[i]); // then store that element in temp
-            }
-            
-            ans.push_back(temp); // push one possible answer string to our answer
-            temp = ""; // again reintiliaze temp for new possible answers
-            i = j; // and move i to j for a fresh start
         }
-        
-        return ans; // and at last finally return the answer array
+        return v;
     }
 };
