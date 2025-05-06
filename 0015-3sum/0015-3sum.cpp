9baff6b17c070348d78dc4f1e373b3befde1da9d
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-05-06 23:30:54 +0600

    Time: 2791 ms (5.01%), Space: 506.3 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end()); // Step 1: Sort the array
-        set<vector<int>> st; // To store unique triplets
-        
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                int x = -(nums[i] + nums[j]); // Find the third number
-                
-                if (binary_search(nums.begin() + j + 1, nums.end(), x)) {
-                    st.insert({nums[i], nums[j], x});
+    vector<vector<int>> threeSum(vector<int>& v) {
+        set<vector<int>> st;
+        vector<vector<int>> ans;
+        sort(v.begin(), v.end());
+        int n = v.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int s = -(v[i] + v[j]);
+                vector<int> vv;
+                if (binary_search(v.begin() + j + 1, v.end(), s)) {
+                    st.insert({v[i], v[j], s});
                 }
             }
         }
-
-        return vector<vector<int>>(st.begin(), st.end());
+        for (auto u : st) {
+            ans.push_back(u);
+        }
+        return ans;
     }
 };
