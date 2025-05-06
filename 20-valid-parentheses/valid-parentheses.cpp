ad30ff7f17052699f707dc08bf96fd7b1fd54c89
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-05-07 00:02:18 +0600

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.55%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> ss;
-        char c = ' ';
+        stack<char> t;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ')') {
-                if (ss.empty())
-                    return false;
-                c = ss.top();
-                if (c != '(')
-                    return false;
-                ss.pop();
-            } else if (s[i] == '}') {
-                if (ss.empty())
-                    return false;
-                c = ss.top();
-                if (c != '{')
-                    return false;
-                ss.pop();
-            } else if (s[i] == ']') {
-                if (ss.empty())
-                    return false;
-                c = ss.top();
-                if (c != '[')
-                    return false;
-                ss.pop();
-            } else
-                ss.push(s[i]);
+            char x = s[i];
+            if (x == '(' || x == '[' || x == '{')
+                t.push(x);
+            else if (!t.empty()) {
+                if (x == ')') {
+                    if (!t.empty() and t.top() != '(')
+                        return false;
+                    else
+                        t.pop();
+                } else if (x == ']') {
+                    if (!t.empty() and t.top() != '[')
+                        return false;
+                    else
+                        t.pop();
+                } else if (x == '}') {
+                    if (!t.empty() and t.top() != '{')
+                        return false;
+                    else
+                        t.pop();
+                }
+            }
+            else return false;
         }
-
-        return ss.empty();
+        if (t.size())
+            return false;
+        return true;
     }
 };
