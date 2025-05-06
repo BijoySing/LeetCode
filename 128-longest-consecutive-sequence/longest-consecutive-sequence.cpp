82305ea1d65ccbd551a452f6d644d3d2a58bb48b
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-05-06 13:28:47 +0600

    Time: 145 ms (13.28%) | Memory: 91.1 MB (17.95%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        set<int>s;
+        int n=nums.size();
+        int pre=INT_MIN;
+        for(auto u:nums)s.insert(u);
+        int ans=0,cnt=0;
+        for(auto u:s){
+          if(u!=pre+1){
+            ans=max(ans,cnt),cnt=0;
+          }
+          cnt++;
+          pre=u;
+        }
+        ans=max(ans,cnt);
+        return ans;
 
-        int n = nums.size();
-        map<int, int> mp;
-        vector<int> v;
-        for (auto u : nums) {
-            mp[u]++;
-        }
-        int mx = 0;
-        for (auto [x, y] : mp) {
-            v.push_back(x);
-            // cout<<x<<" ";
-        }
-        cout<<endl;
-        if (v.size() == 1)
-            return 1;
-        int j = 1, d;
-        for (int i = 0; i < v.size();) {
-            if (j < v.size())
-                d = v[j] - v[i];
-            // cout << v[j] << " " << v[i] << endl;
-            if (d == j - i) {
-                mx = max(d + 1, mx);
-                j++;
-            } else {
-                i++;
-            }
-            // cout << "mx " << mx << endl;
-        }
-        return mx;
     }
 };
