ca2aa535648b9f345e18a6278b89f1172b2c3474
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-05-07 12:12:33 +0600

    Time: 0 ms (100.00%) | Memory: 24.9 MB (5.88%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+       vector<int>v;
         int n = nums.size();
         int c = 0;
         for (int i = 0; i < n; i++) {
 
             if (nums[i]) {
-                nums[c++] = nums[i];
+                v.push_back(nums[i]);c++;
             }
         }
-        while (c< n)nums[c]=0,c++;
+        while (c< n)v.push_back(0),c++;
+        nums.clear();
+        nums=v;
     }
 };
