deccc6faa109591a782c0f8fa17633d108d4cb0e
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-05-09 12:24:01 +0600

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.60%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        int s[n + 1];
+
+        s[1] = 1;
+        s[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            s[i] = s[i - 1] + s[i - 2];
+        }
+        return s[n];
+    }
+};
